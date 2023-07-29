33f817fa835605b33a7c01e95c299444bfd6d406
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-07-29 12:04:33 -0400

    Minimal repro

---
+++ Library/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+
+namespace Library
+{
+	/// <summary>
+	/// File logging configuration options.
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Default value for <see cref="LogLevel"/>.
+		/// </summary>
+		const LogLevel DefaultLogLevel = LogLevel.Debug;
+
+		/// <summary>
+		/// The minimum <see cref="Microsoft.Extensions.Logging.LogLevel"/> to display in logs.
+		/// </summary>
+		[JsonProperty]
+		LogLevel LogLevel { get; set; }
+
+		/// <summary>
+		/// Program entrypoint.
+		/// </summary>
+		public static void Main()
+		{
+		}
+	}
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Library;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			new Program();
+			Program.Main();
+		}
+	}
+}
